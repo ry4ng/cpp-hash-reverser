6f2820c8edc183a99d2d7c0707a1e16609c3b4f4
Ryan <21359049+ry4ng@users.noreply.github.com>
2024-10-06 13:37:31 +0100

    wip

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 
 std::string hash(std::string plaintext);
+void displayCounter(std::vector<int> counter);
+
 char keyspace[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"£$%^&*()_+-=[]{};'#:@~\\|,<.>/?";
 
 int main(int argc, char *argv[]) {
@@ -25,27 +27,31 @@ int main(int argc, char *argv[]) {
     // char keyspace[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"£$%^&*()_+-=[]{};'#:@~\\|,<.>/?";
 
     // 2nd attempt 
-    int maxLenth = 3; 
+    int maxLenth = 3;
     for (int length = 1;length <= maxLenth; ++length) {
-        std::cout << "Length: " << length << std::endl;
+        std::vector<int> counter(length, 0); 
 
-        std::vector<int> indicies(length, 0); 
-        // std::cout << "indicies: " << indicies.size() << std::endl;
+        std::cout << "=============\nLength: " << length << std::endl;
+        // std::cout << "counter: " << counter.size() << std::endl;
+        // displayCounter(counter);
 
         while (true) {
             std::string currentString; 
+            // std::cout << "entering 3rd loop" << std::endl;
             for (int i = 0; i < length; i++) {
-                currentString += keyspace[indicies[i]];
+                std::cout << "i: " << i << std::endl;
+                currentString += keyspace[counter[i]];
+                displayCounter(counter);
             }
             std::cout << currentString << std::endl;
 
             int pos = length - 1;
             std::cout << "pos: " << pos << std::endl;
             while(pos >= 0) {
-                indicies[pos] = indicies[pos] + 1; 
+                counter[pos] = counter[pos] + 1; 
 
-                if (indicies[pos] == strlen(keyspace)) {
-                    indicies[pos] = 0;
+                if (counter[pos] == strlen(keyspace)) {
+                    counter[pos] = 0;
                     pos--;
                 } else {
                     break;
@@ -57,8 +63,8 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        // for (int i = 0; i < indicies.size(); ++i) {
-        //     std::cout << indicies[i] << std::endl;
+        // for (int i = 0; i < counter.size(); ++i) {
+        //     std::cout << counter[i] << std::endl;
         // }
 
         std::cout << "" << std::endl;
@@ -108,4 +114,17 @@ std::string hash(std::string plaintext) {
     sha256.update(reinterpret_cast<const uint8_t*>(plaintext.c_str()), plaintext.length());
     std::string hash = sha256.digest();
     return hash;
+}
+
+void displayCounter(std::vector<int> counter) {
+    // Display content of the counter 
+    std::cout << "[";
+    for (int i = 0; i < counter.size(); ++i) {
+        std::cout << counter[i];
+        // If it's not the last element, print a comma and space
+        if (i < counter.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
 }
